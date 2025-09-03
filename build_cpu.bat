23a2454a70972b3890f71cdeb73cc806222c5405
hgmzhn <hgmzhn@gmail.com>
2025-09-03 23:25:38 +0800

    feat: 添加 PyInstaller 打包配置以创建一键安装包
    为项目引入了使用 PyInstaller 创建 CPU 和 GPU 版本一键安装包所需的配置和脚本。
    
    - 添加 `requirements_cpu.txt` 和 `requirements_gpu.txt`，用于管理不同目标环境的依赖。
    - 创建了一个详细的 `build.spec` 文件，以处理复杂的项目结构、数据文件和隐藏的导入模块。
    - 提供了 `build_cpu.bat` 和 `build_gpu.bat` 脚本，以自动化各个版本的构建过程。
    - 更新了示例配置文件 `config-example.json`，使用相对路径作为默认输出目录。
    - 添加了 `.gitignore` 文件，以排除缓存文件和虚拟环境，保持仓库整洁。

---
+++ build_cpu.bat
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:36e9acb4e9fa9488d8426ec8288fe7e9b7bba86808668f981f77cc18d35d251d
+size 449
---
+++ build_gpu.bat
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3ffea1c00f3e3d0e946b82393f4d54aa3cb90418beae5113adf1db2ea361f429
+size 617
